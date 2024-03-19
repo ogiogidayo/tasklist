3b9aefc0a81ddff926c63197a7cbf8ff1606ab49
ogiwara <146042372+ogiogidayo@users.noreply.github.com>
2024-03-19 16:58:49 +0900

    feat: add record

--- src/main/java/jp/gihyo/projava/tasklist/HomeController.java
+++ src/main/java/jp/gihyo/projava/tasklist/HomeController.java
@@ -5,9 +5,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
+
+    record TaskItem(String id, String task, String deadline, boolean done) {}
+    private List<HomeRestController.TaskItem> taskItems = new ArrayList<>();
+
     @RequestMapping("/hello")
     String hello(Model model) {
         model.addAttribute("time", LocalDateTime.now());
