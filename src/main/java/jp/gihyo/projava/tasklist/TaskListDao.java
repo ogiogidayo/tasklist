a0387b08bf443e84a16b357dc4870e5787048a01
ogiwara <146042372+ogiogidayo@users.noreply.github.com>
2024-03-20 11:50:33 +0900

    feat: db apply

---
+++ src/main/java/jp/gihyo/projava/tasklist/TaskListDao.java
@@ -0,0 +1,45 @@
+package jp.gihyo.projava.tasklist;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Service;
+
+import java.awt.*;
+import java.util.List;
+import java.util.Map;
+
+import static jp.gihyo.projava.tasklist.HomeController.*;
+
+@Service
+public class TaskListDao {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    TaskListDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void add(TaskItem taskItem) {
+        SqlParameterSource param = new BeanPropertySqlParameterSource(taskItem);
+        SimpleJdbcInsert insert = new SimpleJdbcInsert(jdbcTemplate).withTableName("tasklsit");
+        insert.execute(param);
+    }
+
+    public List<TaskItem> findAll() {
+        String query = "SELECT * FROM tasklist";
+
+        List<Map<String, Object>> result = jdbcTemplate.queryForList(query);
+        List<TaskItem> taskItems = result.stream()
+                .map((Map<String, Object> row) -> new TaskItem(
+                        row.get("id").toString(),
+                        row.get("task").toString(),
+                        row.get("deadline").toString(),
+                        (boolean)row.get("done")))
+                .toList();
+
+        return taskItems;
+    }
+}
